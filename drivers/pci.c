33caa8aef3134459900384c7460cd968c36ec3cd
Abizer Lokhandwala <abizer@192>
2025-06-23 19:10:06 +0530

    dumb mf didnt test the build before commiting idk why this is my first proj im still learning

--- drivers/pci.c
+++ drivers/pci.c
@@ -1,5 +1,5 @@
 #include "pci.h"
-
+#include "io.h"
 uint16_t pciReadWord(uint8_t bus ,uint8_t device, uint8_t func, uint8_t reg){
     uint32_t addr;
     uint32_t lbus = (uint32_t)bus; //8 bits for bus
@@ -23,9 +23,10 @@ uint16_t pciReadWord(uint8_t bus ,uint8_t device, uint8_t func, uint8_t reg){
     address | (80000000) just need to set the first bit we can also do |(1<<32)
     address | (reg&252)register has to have the last 2 bits 0 we can also do (reg&0xfc) 
     */
-    address = (uint32_t)((lbus << 16) | (lslot << 11) | (lfunc << 8) | (reg & 0xFC) | ((uint32_t)0x80000000));
+    addr = (uint32_t)((lbus << 16) | (ldev << 11) | (lfunc << 8) | (reg & 0xFC) | ((uint32_t)0x80000000));
     
-    port_byte_out(0xCF8,address);
-    ret = (uint16_t)((port_byte_in(0xCFC)>>((offset&2)*8)) & 0xFFFF);//so we may wanto to read either the first or second 16 bits the offset depending on the register offset so if offset is 1 we read the 16 msb but if its 0 we read the 16 lsb  
+    port_byte_out(0xCF8,addr);
+    ret = (uint16_t)((port_byte_in(0xCFC)>>((reg&2)*8)) & 0xFFFF);
+    //so we may wanto to read either the first or second 16 bits the offset depending on the register offset so if offset is 1 we read the 16 msb but if its 0 we read the 16 lsb  
     return ret;
 }
